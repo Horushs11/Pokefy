848ce8c52ebe6f54bb884caed65aba634f9a850b
Horushs11 <soyroberto2002@gmail.com>
2025-03-31 16:16:25 +0200

    [ADD] Analytics vercel añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // src/App.jsx
 import PokemonList from './components/PokemonList';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
     return (
         <div className="min-h-screen bg-[#e3f2fd]">
+            <Analytics />
             <PokemonList />
         </div>
     );
